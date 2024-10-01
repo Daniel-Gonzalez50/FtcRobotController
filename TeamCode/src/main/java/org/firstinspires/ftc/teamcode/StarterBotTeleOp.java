b55a1eab354b14fbda9c96682f70396d19c357ce
Daniel-Gonzalez50 <danieleg928@gmail.com>
2024-09-30 17:54:56 -0700

    Added all import statements
    Im committing this so that way Nathan can view and add changes to this code and assists in making it go faster.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBotTeleOp.java
@@ -1,3 +1,23 @@
-/* Test if commit works and Nathan can view this
+package org.firstinspires.ftc.teamcode.TeleOp;
 
- */
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
+@TeleOp(name = "Starter Bot TeleOp",group = "Main")
+//@Disabled
+public class StarterBotTeleOp extends LinearOpMode{
+
+    DcMotor frontLeftMotor = null;
+
+    
+}
