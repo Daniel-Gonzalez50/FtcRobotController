6d896949b4594e4f6fabda0df374ef617954d6ae
Daniel-Gonzalez50 <danieleg928@gmail.com>
2024-09-30 17:03:33 -0700

    Repurposing exersises
    Using this to program the motor instead of having the exersises

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBotTeleOp.java
@@ -3,15 +3,14 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@Override
-public class GamepadOpMode extends OpMode {
+@TeleOp()
+public class StarterBotTeleOp {
     @Override
-    public void init(){
+    public class init(){
+
     }
 
-    @Override
     public void loop(){
-        telemetry.addData("Left stick x", )
-    }
 
+    }
 }
